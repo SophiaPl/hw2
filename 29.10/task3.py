3b96ea3111e93950859fb3d63dac4ae69540a5b1
Sophia <93370523+SophiaPl@users.noreply.github.com>
2021-11-01 17:10:59 +0300

    Update task3.py

--- 29.10/task3.py
+++ 29.10/task3.py
@@ -1,6 +1,5 @@
 number = int(input('Введите число > 2: '))
-previous = number - 1
-for i in range(previous, 1, -1):
+for i in range((number - 1), 1, -1):
     for j in range(2, number):
         if i % j != 0:
             continue
