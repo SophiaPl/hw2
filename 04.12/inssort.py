3622fbb724b43e72913e3a63382e7cc86afc7774
Sophia <93370523+SophiaPl@users.noreply.github.com>
2021-12-09 00:07:44 +0300

    Add files via upload

---
+++ 04.12/inssort.py
@@ -0,0 +1,16 @@
+def inssort(nums):
+    for i in range(1, len(nums)):
+        k = nums[i]
+        for j in range(i - 1, -1, -1):
+            l = nums[j]
+            if k >= l:
+                break
+            else:
+                nums[j + 1], nums[j] = l, k
+    return nums
+
+
+# проверка
+if __name__ == '__main__':
+    l = list(map(int, input('Введите список: ').split()))
+    print(inssort(l))
