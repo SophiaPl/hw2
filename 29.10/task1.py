016f40c5bd2b26b99365f15e9a206d82334d790e
Sophia <93370523+SophiaPl@users.noreply.github.com>
2022-01-07 20:28:22 +0300

    Delete 29.10 directory

--- 29.10/task1.py
+++
@@ -1,2 +0,0 @@
-number = int(input('Введите число: '))
-print(f'Число {number} чётное - ' + str(number % 2 == 0))
--- 29.10/task2.py
+++
@@ -1,3 +0,0 @@
-number1 = int(input('Введите первое число: '))
-number2 = int(input('Введите второе число: '))
-print(number1 + number2)
