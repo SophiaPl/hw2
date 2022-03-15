925e2561e8c8f574127f99c7ee3900c434f82d73
Sophia <93370523+SophiaPl@users.noreply.github.com>
2022-03-16 01:25:33 +0300

    Delete 1.py

--- 1.py
+++
@@ -1,8 +0,0 @@
-filename = input('Input genome filename in FASTA format: ')
-
-with open(filename, 'r') as file:
-    for raw in file:
-        if '>' in raw:
-            print('\n', raw.strip())
-        else:
-            print(raw.strip(), end='')
