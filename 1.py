cea09fada6d1dfcf1a5fb2faf41a3228ba8b25b8
Sophia <93370523+SophiaPl@users.noreply.github.com>
2022-03-16 22:50:37 +0300

    Delete 1.py

--- 1.py
+++
@@ -1,8 +0,0 @@
-filename = input('Input genome filename in FASTA format: ')
-
-with open(filename, 'r') as file:
-    for row in file:
-        if '>' in row:
-            print('\n', row.strip())
-        else:
-            print(row.strip(), end='')
